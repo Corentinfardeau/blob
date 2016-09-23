3965e8f5d890beffa7b3e3114561692bd4851020
corentin <fardeaucorentin@gmail.com>
2016-09-22 19:00:34 -0700

    Add popup circle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -17,17 +17,19 @@ class App{
 	
 	constructor(){
 		
-		_.bindAll(this, 'onResize', 'onMousemove', 'onClick', 'draw', 'init');
+		_.bindAll(this, 'onResize', 'onMousemove', 'onClick', 'draw', 'onTouch', 'init');
 		
 		this.ui = this.ui();
 		this.context = this.ui.canvas.getContext('2d');
 
 		this.params = {
-			radius : 200,
-			nbPoints : 100,
+			radius : 100,
+			nbPoints : 50,
 			spring: 0.03,
 			debug : false
 		};
+
+		this.isFirst = true;
 
 		this.circles = [];
 				
@@ -36,28 +38,125 @@ class App{
 			y: window.innerHeight/2
 		}
 
-		this.gui = new dat.GUI();
-		this.initDat();
-		this.init();
-		window.addEventListener('resize', this.onResize);
-		window.addEventListener('click', this.onClick);
-		window.addEventListener('mousemove', this.onMousemove);
+		// this.gui = new dat.GUI();
+		// this.initDat();
+
+		//REMOVE SCROLL BOUNCE IOS
+		var xStart, yStart = 0;
+ 
+		document.addEventListener('touchstart',function(e) {
+		    xStart = e.touches[0].screenX;
+		    yStart = e.touches[0].screenY;
+		});
+		 
+		document.addEventListener('touchmove',function(e) {
+		    var xMovement = Math.abs(e.touches[0].screenX - xStart);
+		    var yMovement = Math.abs(e.touches[0].screenY - yStart);
+		    if((yMovement * 3) > xMovement) {
+		        e.preventDefault();
+		    }
+		});
+
+		this.animateIn();
+
 	}
 	
 	ui(){
 		return {
-			canvas: document.querySelector('.canvas')
+			canvas: document.querySelector('.canvas'),
+			title: document.querySelector('.loader h1'),
+			spinner: document.querySelector('.spinner-container'),
+			loaderInfos: document.querySelector('.loader p'),
+			loader: document.querySelector('.loader'),
+			contentTitle: document.querySelector('.content h2'),
+			contentInfos: document.querySelector('.content p'),
+			back: document.querySelector('.content .back')
 		} 	
 	}
-	
-	onMousemove(e){
-	    this.mousePos = {
-	    	x: e.pageX,
-	    	y: e.pageY
-	    }
+
+	addEvent(){
+		window.addEventListener('resize', this.onResize);
+		window.addEventListener('click', this.onClick);
+		window.addEventListener('touchstart',this.onTouch);
+		window.addEventListener('mousemove', this.onMousemove);
+	}
+
+	animateIn(){
+
+		let tl = new TimelineMax({delay: 0.5});
+
+		tl.fromTo(this.ui.title, 1, {
+			x: -20,	
+			force3D: true,
+			alpha: 0
+		},{
+			alpha: 1,
+			x: 0,
+			ease: Quint.easeOut
+		}, 0);
+
+		tl.fromTo(this.ui.spinner, 1, {
+			x: -20,	
+			force3D: true,
+			opacity: 0
+		},{
+			opacity: 1,
+			x: 0,
+			ease: Quint.easeOut
+		}, 0.1);
+
+
+		tl.fromTo(this.ui.loaderInfos, 1, {
+			y: 10,	
+			force3D: true,
+			opacity: 0
+		},{
+			opacity: 0.3,
+			y: 0,
+			ease: Quint.easeOut
+		}, 0.5);
+
+		setTimeout(this.removeLoader.bind(this), 3500)
+
+	}
+
+	removeLoader(){
+		let tl = new TimelineMax({
+			onComplete: this.init,
+			onCompleteScope: this
+		});
+
+
+		tl.to(this.ui.loaderInfos, 1, {
+			opacity: 0,
+			y: 10,
+			ease: Quint.easeIn
+		}, 0);
+
+
+		tl.fromTo(this.ui.spinner, 1, {
+			x: 0,
+			force3D: true,
+			opacity: 1
+		},{
+			opacity: 0,
+			x: 20,
+			ease: Quint.easeIn
+		}, 0.3);
+
+		tl.fromTo(this.ui.title, 1, {
+			x: 0,	
+			force3D: true,
+			alpha: 1
+		},{
+			alpha: 0,
+			x: 20,
+			ease: Quint.easeIn
+		}, 0.4);
 	}
 
 	init(){
+
 		this.onResize();
 
 		let grd = this.context.createLinearGradient(this.center.x,this.center.y,170,0);
@@ -70,7 +169,7 @@ class App{
 		}, grd);
 
 		this.circles.push(circle);
-		//this.noise = new Noise(this.context);
+		this.noise = new Noise(this.context);
 
 		TweenMax.ticker.addEventListener('tick', this.draw);
 	}
@@ -111,6 +210,27 @@ class App{
 		this.circles.push(circle);
 	}
 
+	onTouch(e){
+		console.log();
+		let grd2 = this.context.createLinearGradient(this.center.x,this.center.y,170,0);
+		grd2.addColorStop(0,"#ddd6f3");
+		grd2.addColorStop(1,"#faaca8");
+
+		let circle = new Circle(this.context, this.params, {
+			x: e.changedTouches[0].clientX,
+			y: e.changedTouches[0].clientY
+		}, grd2);
+
+		this.circles.push(circle);
+	}
+
+	onMousemove(e){
+	    this.mousePos = {
+	    	x: e.pageX,
+	    	y: e.pageY
+	    }
+	}
+
 	initDat(){
 		this.gui.add(this.params, 'debug').onChange(this.init);
 		this.gui.add(this.params, 'nbPoints', 3, 500).onChange(this.init);
@@ -135,8 +255,27 @@ class App{
 		}
 
 		this.drawCursor();
-
 		this.noise.update();
+		
+		if(this.isFirst){
+			
+			this.isFirst = false;
+			this.addEvent();
+
+			TweenMax.staggerFromTo([this.ui.contentTitle,this.ui.contentInfos, this.ui.back], 1,{
+				y: 10,
+				alpha: 0
+			},{
+				y: 0,
+				alpha: 1,
+				ease: Quint.easeOut
+			}, 0.1, 0);
+
+			TweenMax.set(this.ui.loader, {
+				autoAlpha: 0
+			});
+		}
+
 	}
 	
 }
@@ -150,13 +289,19 @@ class App{
 class Circle{
 
 	constructor(context, params, center, grd){
+
+		_.bindAll(this, 'updateAnimateIn');
+
 		this.params = params;
 		this.context = context;
 		this.center = center;
 		this.points = [];
 		this.grd = grd;
+		this.inactive = true;
+		this.angleAnimateIn = this.params.radius * Math.abs(Math.cos(-1.2));
 
 		this.drawCircle();
+		this.animateIn();
 	}
 
 	drawCircle(){
@@ -176,34 +321,69 @@ class Circle{
 
 	}
 
+	animateIn(){
+
+		let radius = {
+			scale : 0
+		}
+
+		TweenMax.to(radius, 0.8, {
+			scale: this.params.radius,
+			ease: Elastic.easeOut.config(1, 0.5),
+			onUpdate: this.updateAnimateIn,
+			onUpdateParams:[radius],
+			onComplete: function(){
+				this.inactive = false
+			}.bind(this)
+		});
+
+	}
+
+	updateAnimateIn(radius){
+		this.animateInRadius = radius;
+	}
+
 	update(){
+		if(!this.inactive){		
+			this.context.fillStyle = this.grd;
+			this.context.shadowBlur = 500;
+			this.context.shadowColor = "#003C65";
+			this.context.beginPath();
 
-		this.context.fillStyle = this.grd;
-		this.context.shadowBlur = 500;
-		this.context.shadowColor = "#003C65";
-		this.context.beginPath();
-
-		for (var i = 0; i < this.points.length - 1; i++) {
-			this.context.lineTo(this.points[i].x, this.points[i].y, this.points[i+1].x, this.points[i+1].y);
-		}
-
-		this.context.closePath();
-		this.context.fill();
-
-
-		for (var i = 0; i < this.points.length; i++) {
-			this.points[i].update();
-			if (this.params.debug){
-				this.context.fillStyle = '#3370d4';
-				this.context.beginPath();
-				this.context.arc(this.points[i].x, this.points[i].y, 4, 0, 2 * Math.PI, false);
-				this.context.closePath();
-				this.context.fill();
+			for (var i = 0; i < this.points.length - 1; i++) {
+				this.context.lineTo(this.points[i].x, this.points[i].y, this.points[i+1].x, this.points[i+1].y);
 			}
+
+			this.context.closePath();
+			this.context.fill();
+
+
+			for (var i = 0; i < this.points.length; i++) {
+				this.points[i].update();
+				if (this.params.debug){
+					this.context.fillStyle = '#3370d4';
+					this.context.beginPath();
+					this.context.arc(this.points[i].x, this.points[i].y, 4, 0, 2 * Math.PI, false);
+					this.context.closePath();
+					this.context.fill();
+				}
+			}
+
+			this.drawCenter();
+
+		} else {
+
+			this.context.fillStyle = this.grd;
+			this.context.shadowBlur = 500;
+			this.context.shadowColor = "#003C65";			
+			this.context.beginPath();
+
+			this.context.arc(this.center.x, this.center.y, this.animateInRadius.scale, 0, Math.PI * 2, false);
+
+			this.context.closePath();
+			this.context.fill();
+
 		}
-
-		this.drawCenter();
-
 	}
 
 	drawCenter(){
@@ -216,6 +396,9 @@ class Circle{
 		}
 	}
 
+	getAngle(a, b){
+		return Math.atan2(a.y - b.y, a.x - b.x);
+	}
 }
 
 /**
@@ -228,7 +411,8 @@ class Point{
 	
 	constructor(x, y, params, context, originCenter){
 
-		_.bindAll(this, 'onMousemove');
+		_.bindAll(this, 'onTouchMove', 'onMousemove');
+
 		this.ox = x;
 		this.oy = y;
 		this.x = x;
@@ -245,12 +429,21 @@ class Point{
 		this.originCenter = originCenter;
 
 		window.addEventListener('mousemove', this.onMousemove);
+		window.addEventListener('touchmove', this.onTouchMove);
+
 	}
 	
 	onMousemove(e){
 	    this.mousePos = {
 	    	x: e.pageX,
 	    	y: e.pageY
+	    }
+	}
+
+	onTouchMove(e){
+	    this.mousePos = {
+	    	x: e.targetTouches[0].pageX,
+	    	y: e.targetTouches[0].pageY,
 	    }
 	}
 
@@ -296,6 +489,7 @@ class Point{
 class Noise{
 
 	constructor(){
+
 		var canvas = document.querySelector('.noise-canvas');
 		this.context = canvas.getContext('2d');
 
